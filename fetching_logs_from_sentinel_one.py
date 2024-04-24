154035dee94cc78cda58db91f115f2585a153a72
Tinh Phan <pttinh0804@gmail.com>
2024-04-24 22:58:41 +0700

    feat: improve error handling

---
+++ fetching_logs_from_sentinel_one.py
@@ -0,0 +1,200 @@
+import requests
+import time
+import sys
+import json
+from datetime import datetime, timedelta
+import concurrent.futures
+
+api_token = 'eyJraWQiOiJ0b2tlblNpZ25pbmciLCJhbGciOiJFUzI1NiJ9.eyJzdWIiOiJ2b3ZhbmtoYW5oQHplbml0ZWNoY3MuY29tIiwiaXNzIjoiYXV0aG4tdXMtZWFzdC0xLXByb2QiLCJkZXBsb3ltZW50X2lkIjoiNzE4MjIiLCJ0eXBlIjoidXNlciIsImV4cCI6MTcxNTQyMTcxMCwianRpIjoiNjEyNzI2NmMtMDUzNy00MzJkLWIzYzEtODQ0ZDZhY2EzZDQwIn0.mg1RZqZ1swfjETKfZqd9L55NcG__xkD4oRfZwknmLmBhbleFptVAzj8J7NTQY52nB67vNgGeN7z_jP3_piVhtA'
+headers = {'Authorization': f'ApiToken {api_token}'}
+
+FINISHED = "FINISHED"
+TIME_RANGE = 30
+
+def terminal_request_id(query_id):
+    url = 'https://usea1-016.sentinelone.net/web/api/v2.1/dv/cancel-query'
+    payload = {"queryId": query_id}
+    headers = {'Authorization': f'ApiToken {api_token}'}
+    # time.sleep(1)
+    try:
+        response = requests.post(
+            url, json=payload, headers=headers)  # Indicate success
+    except requests.exceptions.Timeout:
+        return terminal_request_id(query_id)
+    except requests.exceptions.RequestException as e:
+        return terminal_request_id(query_id)
+    if response.status_code == 200:
+       # print("success")
+        return 1  # Indicate success
+    return -1  # Indicate failure
+
+# read data to json
+def read_data_json(data, toDate):
+    try:
+        # current_time = datetime.now()
+        # Convert the timestamp to a human-readable format for the filename
+        # formatted_time = toDate.strftime('%Y-%m-%d_%H')
+        toDate_datetime = datetime.strptime(toDate, '%Y-%m-%dT%H:%M:%S.%fZ')
+        formatted_time = toDate_datetime.strftime('%Y-%m-%d_%H')
+        # Construct the filename
+        # filename = f'./logs/events_{toDate}.log'
+        filename = f'events_{formatted_time}.log'
+        print("Writing events to file")
+        for obj in data['data']:
+            # Write the JSON object to the file
+            with open(filename, 'a') as file:
+                file.write(json.dumps(obj) + '\n')
+    except IOError as e:
+        print("Error writing to file:", e)
+
+
+def increase_time_interval(to_date, time_range):
+    new_to_date = calculate_new_to_date(to_date, time_range)
+    return to_date, new_to_date
+
+def calculate_new_to_date(from_date, time_range):
+    to_date = datetime.strptime(
+        from_date, "%Y-%m-%dT%H:%M:%S.%fZ") + timedelta(seconds=time_range)
+    return to_date.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+
+# Function to send POST request and extract queryId
+def initiate_query(from_date, to_date):
+    log_prefix = 'initiate_query'
+    url = 'https://usea1-016.sentinelone.net//web/api/v2.1/dv/init-query'
+    payload = {
+        "accountIds": ["1530556909291787901"],
+        "siteIds": ["1530556911045007055"],
+        "fromDate": from_date,
+        "limit": 20000,
+        "queryType": ["events"],
+        "toDate": to_date,
+        "isVerbose": "false",
+        # "timeFrame": "Last 100 Hours",
+        "query": "AgentName IS NOT EMPTY",
+
+    }
+
+    print(f'{log_prefix} started to initiate the query and get queryId')
+    response = requests.post(url, json=payload, headers=headers)
+    data = handle_response(response, log_prefix, initiate_query, from_date, to_date)
+    query_id = data['data']['queryId']
+    print(f'{log_prefix} done with returned queryId = {query_id}')
+    return query_id
+
+# Function to send GET request using the queryId
+def fetch_log_events(query_id):
+    log_prefix = 'fetch_log_events'
+    url = f'https://usea1-016.sentinelone.net/web/api/v2.1/dv/events'
+    params = {'queryId': query_id, 'limit': 1000}
+
+    print(f'{log_prefix} started with queryId = {query_id}')
+    response = requests.get(url, params=params, headers=headers)
+    return handle_response(response, log_prefix, fetch_log_events, query_id)
+
+# Function to send GET request using the queryId
+def fetch_log_events_with_cursor(query_id, cursor):
+    log_prefix = 'fetch_log_events_with_cursor'
+    url = f'https://usea1-016.sentinelone.net/web/api/v2.1/dv/events'
+    params = {'queryId': query_id, 'limit': 1000, 'cursor': cursor}
+
+    print(f'{log_prefix} started with query_id = {query_id}')
+    response = requests.get(url, params=params, headers=headers)
+    return handle_response(response, log_prefix, fetch_log_events_with_cursor, query_id, cursor)
+
+def get_query_status(query_id):
+    log_prefix = 'get_query_status'
+    url = 'https://usea1-016.sentinelone.net/web/api/v2.1/dv/query-status'
+    params = {'queryId': query_id}
+
+    print(f'{log_prefix} started getting status for queryId = {query_id}')
+    # Wait for the service to process the query
+    time.sleep(4)
+    res = requests.get(url, params=params, headers=headers)
+    data = handle_response(res, log_prefix, get_query_status, query_id)
+    progress_status, response_state = data['data']['progressStatus'], data['data']['responseState']
+    if response_state != FINISHED:
+        print(f'{log_prefix} query is being processed. Progress Status: {progress_status}')
+        return get_query_status(query_id)
+    else:
+        print(f'{log_prefix} query execution is done. Query results are ready to be fetched')
+        return FINISHED
+
+def handle_response(res, func_name, fun, *args):
+    SHORT_TIME_SLEEP = 5
+    LONG_TIME_SLEEP = 60
+    if res.status_code == 200:
+        print(f'{func_name} done successfully')
+        return res.json()
+    elif res.status_code == 503:
+        print(f'{func_name} got error {res.reason}. Going to wait for the service becomes available')
+        time.sleep(SHORT_TIME_SLEEP)
+        print(f'{func_name} retries after some seconds')
+        return fun(*args)
+    elif res.status_code == 409:
+        print(f'{func_name} got too many requests. Wait 1 min for the service to cool down')
+        if (func_name == "initiate_query"):
+            time.sleep(LONG_TIME_SLEEP)
+        elif (func_name == "get_query_status"):
+            # pass is intended
+            pass
+        else:
+            time.sleep(SHORT_TIME_SLEEP)
+        print(f'{func_name} retries after some times')
+        return fun(*args)
+    else:
+        print(
+            f'Got error in {func_name}: \
+            Reason {res.reason} \
+            Error code: {res.status_code}')
+        raise
+
+# Main function
+def main():
+    # fromDate = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f") + 'Z'
+    # time.sleep(20)
+    # toDate = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f") + 'Z'
+
+    # 9357 items
+    # fromDate = "2024-04-12T01:58:26.257525Z"
+    # toDate = "2024-04-12T01:59:26.257525Z"
+
+    # 20000 items
+    from_date = "2024-04-10T20:58:00.0Z"
+    # toDate = "2024-04-10T20:58:20.0Z"
+    from_date, to_date = increase_time_interval(from_date, TIME_RANGE)
+
+    # checker_f = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f") + 'Z'
+    # checker_f = datetime.strptime(checker_f, "%Y-%m-%dT%H:%M:%S.%fZ")
+    while True:
+        print(f'\nFetching Data from {from_date} to {to_date}')
+
+        query_id = initiate_query(from_date, to_date)
+
+        # Wait for the query to be processed
+        get_query_status(query_id)
+
+        # events are ready
+        data = fetch_log_events(query_id)
+
+        number_items = data['pagination']['totalItems']
+        print(f'Getting {number_items} items in total')
+        while True:
+            read_data_json(data, to_date)
+            cursor = data['pagination']["nextCursor"]
+            if cursor is None:
+                break
+            data = fetch_log_events_with_cursor(query_id, cursor)
+
+        from_date, to_date = increase_time_interval(to_date, TIME_RANGE)
+
+    # checker_t = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f") + 'Z'
+    # checker_t = datetime.strptime(checker_t, "%Y-%m-%dT%H:%M:%S.%fZ")
+    # if (checker_t - checker_f).total_seconds() < 60:
+    #     sleep_time = 60 - (checker_t - checker_f).total_seconds()
+    # # print(sleep_time)
+    #     time.sleep(sleep_time)
+    # # time.sleep(60)  # Wait before sending the next request batch
+
+
+if __name__ == "__main__":
+    main()
--- unformatted_time_to_seconds.py
+++ unformatted_time_to_seconds.py
@@ -1,35 +1,239 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 26 22:05:51 2024
+import requests
+import time
+import sys
+import json
+from datetime import datetime, timedelta
 
-@author: pttin
-"""
+api_token = ''
+# Update time interval every time request a batch fetching data (query ID)
 
-import re
-def unformatted_time_to_seconds(time):
-    time = time.lower()
-    seconds_per_unit = {"s": 1, "sec": 1,
-                        "m": 60, "mins": 60,
-                        "h": 3600, "hrs": 3600,
-                        "d": 86400, "ds": 86400, "days": 86400,
-                        "w": 604800, "wk": 604800, "wks": 604800, 
-                        "mo": 2629744, "mos": 2629744,
-                        "y": 31556926, "yr": 31556926, "yrs": 31556926}
-    total_seconds = 0
-    parsed_time = re.findall(r'\d+', time)
-    parsed_time_units = re.findall(r'[a-z]+', time)
-    if len(parsed_time) != len(parsed_time_units):
-        raise Exception("Can't calculate due to invalid input time: " + time)
-    for i in range(len(parsed_time)):
-        total_seconds += int(parsed_time[i]) * seconds_per_unit[parsed_time_units[i]]  
-    return total_seconds
+headers = {'Authorization': f'ApiToken {api_token}'}
 
-test1 = "3d"
-test2 = "2d 1h"
-test3 = "1w 1d"
-test4 = "3mos 3w"
-test5 = "2d 10h"
-print(unformatted_time_to_seconds(test1))
-print(unformatted_time_to_seconds(test2))
-print(unformatted_time_to_seconds(test3))
-print(unformatted_time_to_seconds(test5))
+FINISHED = "FINISHED"
+TIME_RANGE = 20
+
+
+def update_time_interval(toDate, time_range):
+    newToDate = calculate_to_date(toDate, time_range)
+    return toDate, newToDate, time_range
+
+
+def calculate_to_date(fromDate, time_range):
+    toDate = datetime.strptime(
+        fromDate, "%Y-%m-%dT%H:%M:%S.%fZ") + timedelta(seconds=time_range)
+    return toDate.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+
+# Function to send POST request and extract queryId
+
+
+def send_post_request(fromDate, toDate):
+    url = 'https://usea1-016.sentinelone.net//web/api/v2.1/dv/init-query'
+    payload = {
+        "accountIds": [""],
+        "siteIds": [""],
+        "fromDate": fromDate,
+        "limit": 20000,
+        "queryType": ["events"],
+        "toDate": toDate,
+        "isVerbose": "false",
+        # "timeFrame": "Last 100 Hours",
+        "query": "AgentName IS NOT EMPTY",
+
+    }
+    headers = {'Authorization': f'ApiToken {api_token}'}
+    print("send_post_request started to initate the query and get queryId")
+    response = requests.post(url, json=payload, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        query_id = data['data']['queryId']
+        if query_id:
+            print("send_post_request done")
+            return query_id, fromDate, toDate
+        else:
+            print("Can't get queryId")
+            raise
+    elif response.status_code == 429:
+        print("Too many requests. Wait 1 min for the service to cool down")
+        time.sleep(60)
+        raise
+    else:
+        print(
+            f'Got error in send_post_request: \
+            Reason {response.reason} \
+            Error code: {response.status_code}')
+        raise
+
+
+def terminal_request_id(query_id):
+    url = 'https://usea1-016.sentinelone.net/web/api/v2.1/dv/cancel-query'
+    payload = {"queryId": query_id}
+    headers = {'Authorization': f'ApiToken {api_token}'}
+    # time.sleep(1)
+    try:
+        response = requests.post(
+            url, json=payload, headers=headers)  # Indicate success
+    except requests.exceptions.Timeout:
+        return terminal_request_id(query_id)
+    except requests.exceptions.RequestException as e:
+        return terminal_request_id(query_id)
+    if response.status_code == 200:
+       # print("success")
+        return 1  # Indicate success
+    return -1  # Indicate failure
+
+# read data to json
+
+
+def read_data_json(data, toDate):
+    try:
+        # current_time = datetime.now()
+        # Convert the timestamp to a human-readable format for the filename
+        # formatted_time = toDate.strftime('%Y-%m-%d_%H')
+        toDate_datetime = datetime.strptime(toDate, '%Y-%m-%dT%H:%M:%S.%fZ')
+        formatted_time = toDate_datetime.strftime('%Y-%m-%d_%H')
+        # Construct the filename
+        # filename = f'./logs/events_{toDate}.log'
+        filename = f'events_{formatted_time}.log'
+        print("Writing events to file")
+        for obj in data['data']:
+            # Write the JSON object to the file
+            with open(filename, 'a') as file:
+                file.write(json.dumps(obj) + '\n')
+    except IOError as e:
+        print("Error writing to file:", e)
+# Function to send GET request using the queryId
+
+
+def send_get_request(query_id):
+    url = f'https://usea1-016.sentinelone.net/web/api/v2.1/dv/events'
+    params = {'queryId': query_id, 'limit': 1000}
+    print(f'send_get_request started with queryId = {query_id}')
+    response = requests.get(url, params=params, headers=headers)
+    if response.status_code == 200:
+        print(f'send_get_request with queryId = {query_id} done')
+        return response.json()
+    else:
+        print(
+            f'Got error in send_get_request: \
+            Reason {response.reason} \
+            Error code: {response.status_code}')
+        raise
+
+
+# Function to send GET request using the queryId
+def send_get_request_cursor(query_id, cursor):
+    url = f'https://usea1-016.sentinelone.net/web/api/v2.1/dv/events'
+    params = {'queryId': query_id, 'limit': 1000, 'cursor': cursor}
+
+    print(f'send_get_request_cursor started with query_id = {query_id}')
+    response = requests.get(url, params=params, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()
+    elif response.status_code == 503:
+        print(
+            f'Got error {response.reason}. Going to wait for the service become available')
+        time.sleep(5)
+        return send_get_request_cursor(query_id, cursor)
+    else:
+        print(
+            f'Got error in send_get_request_cursor: \
+            Reason {response.reason} \
+            Error code: {response.status_code}')
+        raise
+
+
+def get_query_status(query_id):
+    url = 'https://usea1-016.sentinelone.net/web/api/v2.1/dv/query-status'
+    params = {'queryId': query_id}
+    print(f'Start getting status for queryId = {query_id}')
+    # Wait for the service to process the query
+    time.sleep(4)
+    res = requests.get(url, params=params, headers=headers)
+    if res.status_code == 200:
+        data = res.json()['data']
+        progressStatus, responseState = data['progressStatus'], data['responseState']
+        if responseState != FINISHED:
+            print(
+                f'Query is being processed. Progress Status: {progressStatus}')
+            return get_query_status(query_id)
+        else:
+            print("Query execution is done. Query results are ready to be fetched")
+            return FINISHED
+    else:
+        print("Something went wrong in get_query_status")
+        raise
+
+# Main function
+
+
+def main():
+    # fromDate = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f") + 'Z'
+    # time.sleep(20)
+    # toDate = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f") + 'Z'
+
+    # 9357 items
+    # fromDate = "2024-04-12T01:58:26.257525Z"
+    # toDate = "2024-04-12T01:59:26.257525Z"
+
+    # 20000 items
+    fromDate = "2024-04-10T20:58:00.0Z"
+    # toDate = "2024-04-10T20:58:20.0Z"
+    toDate = calculate_to_date(fromDate, TIME_RANGE)
+
+    # checker_f = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f") + 'Z'
+    # checker_f = datetime.strptime(checker_f, "%Y-%m-%dT%H:%M:%S.%fZ")
+
+    while True:
+        print('Fetching Data In Range', fromDate, toDate)
+
+        query_id, fromDate, toDate = send_post_request(fromDate, toDate)
+
+        print("Received new queryId:", query_id)
+        get_query_status(query_id)
+
+        # events are ready
+        data = send_get_request(query_id)
+
+        number_items = data['pagination']['totalItems']
+        print(f'Getting {number_items} items in total')
+        while True:
+            read_data_json(data, toDate)
+            cursor = data['pagination']["nextCursor"]
+            if cursor is None:
+                break
+            data = send_get_request_cursor(query_id, cursor)
+
+        fromDate, toDate, time_range = update_time_interval(toDate, TIME_RANGE)
+
+        # else:
+        #     fromDate, toDate, time_range = update_time_interval_again(
+        #         fromDate, toDate, time_range - 15)
+        # data = send_get_request(query_id)
+        # while data['pagination']['totalItems'] == 20000:
+        #  print("Rerequest Query id")
+        # terminal = terminal_request_id(query_id)
+        #     fromDate, toDate, time_range = update_time_interval_again(
+        #         fromDate, toDate, time_range - 15)
+        #     query_id, fromDate, toDate, time_range = send_post_request(
+        #         fromDate, toDate, time_range)
+        #     data = send_get_request(query_id)
+        #     # print("New time number ")
+        #     # print(fromDate, toDate)
+        #     # print(data['pagination']['totalItems'])
+        #     # print("High s1 logs")
+        # read_data_json(data, toDate)
+
+        #terminal = terminal_request_id(query_id)
+        # checker_t = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f") + 'Z'
+        # checker_t = datetime.strptime(checker_t, "%Y-%m-%dT%H:%M:%S.%fZ")
+        # if (checker_t - checker_f).total_seconds() < 60:
+        #     sleep_time = 60 - (checker_t - checker_f).total_seconds()
+        # # print(sleep_time)
+        #     time.sleep(sleep_time)
+        # # time.sleep(60)  # Wait before sending the next request batch
+
+
+if __name__ == "__main__":
+    main()
